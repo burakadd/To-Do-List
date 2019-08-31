bc972d62f2fee929aba2868349a4e976b2891ebf
burakadd <burakad@inbox.ru>
2019-09-01 01:47:45 +0300

    Work with static

--- ToDo/forms.py
+++ ToDo/forms.py
@@ -7,15 +7,3 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ('text', )
-
-    # smtext = forms.CharField(
-    #     max_length=40,
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': 'make zverkova happy',
-    #             'aria-label': 'ToDo',
-    #             'aria-describedby': 'add-btn'
-    #         }
-    #     )
-    # )
--- ToDo/views.py
+++ ToDo/views.py
@@ -1,28 +1,11 @@
-from django.shortcuts import render, redirect
-from django.views.decorators.http import require_POST
 from django.views.generic import FormView
-from django.views.generic.base import ContextMixin
 
 from .models import Todo
 from .forms import TodoForm
-# Create your views here.
-
-
-# def index(request):
-#     todo_list = Todo.objects.order_by('id')
-#
-#     form = TodoForm()
-#
-#     context = {'todo_list': todo_list, 'form': form}
-#
-#     return render(request, 'ToDo/index.html', context)
 
 
 class MainPage(FormView):
     form_class = TodoForm
-    context = {
-
-    }
 
     def form_valid(self, form):
         form.save()
@@ -33,14 +16,3 @@ class MainPage(FormView):
             'todo_list': Todo.objects.all(),
             'form': self.form_class
         }
-
-
-# @require_POST
-# def addToDo(request):
-#     form = TodoForm(request.POST)
-#
-#     if form.is_valid():
-#         new_todo = Todo(text=request.POST['text'])
-#         new_todo.save()
-#
-#     return redirect('index')
