07e07a245ed095317190b34219ddd54a8415e997
burakadd <6ypu4.92@gmail.com>
2019-09-03 07:33:02 +0300

    in progress

--- ToDo/admin.py
+++ ToDo/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Todo
+
+from .models import Todo, MyUser
 
 admin.site.register(Todo)
+admin.site.register(MyUser)
--- ToDo/models.py
+++ ToDo/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
@@ -9,3 +11,7 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class MyUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
